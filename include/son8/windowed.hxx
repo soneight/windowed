5991af854c09074b15859f81c112781168ab566a
Oleg'Ease'Kharchuk <olegease@gmail.com>
2025-12-22 09:42:34 +0200

    Window 2025 12 22 #17
    * refactor windowed header, add window header
    
    * refactor windowed source
    
    * rename Windowed to Window, minor 3

--- include/son8/windowed.hxx
+++ include/son8/windowed.hxx
@@ -1,40 +1,15 @@
 #ifndef SON8_WINDOWED_HXX
 #define SON8_WINDOWED_HXX
 
-#include "windowed/config.hxx"
+#include <son8/windowed/config.hxx>
+#include <son8/windowed/window.hxx>
 
-#include <memory>
-#include <utility>
-
-// forward declarations
-struct GLFWwindow;
-
+// TODO: remove on release?
 namespace son8::windowed {
-    class Windowed {
-        class Impl_;
-        std::unique_ptr< Impl_ > impl_;
+    // alias so old code would work
+    using Windowed [[deprecated("son8::windowed Windowed class is an old name and subject to be removed, use Window instead")]] = Window;
 
-        bool running( ) const;
-        void process( ) const;
-    public:
-        Windowed( Config const &config = { } );
-        ~Windowed( );
-        Windowed( Windowed&& ) = delete;
-        Windowed( Windowed const& ) = delete;
-        Windowed &operator=( Windowed && ) = delete;
-        Windowed &operator=( Windowed const & ) = delete;
-
-        operator GLFWwindow *( ) const;
-
-        template< typename Callback, typename ...Args >
-        void run( Callback &&callback, Args &&...args ) {
-            while ( running( ) ) {
-                std::forward< Callback >( callback )( std::forward< Args >( args )... );
-                process( );
-            }
-        }
-    }; // class Windowed
-} // namespace son8::windowed
+}
 
 #endif//SON8_WINDOWED_HXX
 
